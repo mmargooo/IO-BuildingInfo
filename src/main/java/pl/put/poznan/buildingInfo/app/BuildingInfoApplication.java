70884d08acd5794f7665dfa233f19d7924b04864
m_graziowski <you@example.com>
2018-11-10 18:37:14 +0100

    feature/get-structures delete redundant blank line

--- src/main/java/pl/put/poznan/buildingInfo/app/BuildingInfoApplication.java
+++ src/main/java/pl/put/poznan/buildingInfo/app/BuildingInfoApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BuildingInfoApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(BuildingInfoApplication.class, args);
     }
 }
