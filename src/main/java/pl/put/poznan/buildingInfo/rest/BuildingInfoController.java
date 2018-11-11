3db90a5f26f2c94182a519f852bb5f545fccc0b1
m_graziowski <you@example.com>
2018-11-11 14:48:07 +0100

    feature/sum-the-area, add gson object as a field of controller, create endpoint for calculating area, implement getArea in building Class and level Class

--- src/main/java/pl/put/poznan/buildingInfo/rest/BuildingInfoController.java
+++ src/main/java/pl/put/poznan/buildingInfo/rest/BuildingInfoController.java
@@ -3,10 +3,7 @@ package pl.put.poznan.buildingInfo.rest;
 import com.google.gson.Gson;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.put.poznan.buildingInfo.logic.BuildingInfo;
 import pl.put.poznan.buildingInfo.model.Building;
 import pl.put.poznan.buildingInfo.model.Location;
@@ -18,11 +15,11 @@ import java.util.HashMap;
 @RequestMapping("/api")
 public class BuildingInfoController {
     private static final Logger logger = LoggerFactory.getLogger(BuildingInfoController.class);
+    private static final Gson gson = new Gson();
 
     @RequestMapping(value = "/new", method = RequestMethod.POST, produces = "application/json")
     public String createBuilding(@RequestBody String payload) {
         HashMap<String, Location> locations = new HashMap<>();
-        Gson gson = new Gson();
         Building building = gson.fromJson(payload, Building.class);
         locations.put(building.getId(), building);
         building.getLevels().forEach(level -> {
@@ -32,6 +29,15 @@ public class BuildingInfoController {
         BuildingInfo.setLocations(locations);
         return gson.toJson("Building " + building.getId() + " successfully created");
     }
+
+    @RequestMapping(value = "/area/{id}", method = RequestMethod.GET, produces = "application/json")
+    public String getArea(@PathVariable String id) {
+        logger.debug("getArea " + id);
+        Location location = BuildingInfo.getLocation(id);
+        if (location == null) {
+            logger.debug("Such location doesnt exist");
+            return "{\"status\": \"failure\", \"value\":\"Location doesnt exist\"}";
+        }
+        return "{\"status\": \"success\", \"value\": \"" + location.getArea() + "\"}";
+    }
 }
-
-
