ffc527be3604a999c35ad1b1b53f8fc7a14242f8
StanislawSzataniak <stanislaw.szataniak17@gmail.com>
2018-12-02 15:05:11 +0100

    feature/implement-visitor removed redundant lines

--- src/main/java/pl/put/poznan/buildingInfo/model/Building.java
+++ src/main/java/pl/put/poznan/buildingInfo/model/Building.java
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.*;
 import java.util.List;
-import java.util.ArrayList;
-import java.util.stream.Collectors;
 
 /**
  * Represents class describing Building, that consist of Levels
